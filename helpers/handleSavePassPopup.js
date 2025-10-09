4d6f1a59ab5ccee38277d515bf0e73796067dbb3
FDC.Jaime-MT-QA Lead <fdc.jaime-mt-qalead@FDCJaimMTQALead.localdomain>
2025-10-09 17:20:02 +0800

    updated helper to handle popup message for save pass

--- helpers/handleSavePassPopup.js
+++ helpers/handleSavePassPopup.js
@@ -2,7 +2,7 @@ export async function handleSavePass(driver) {
     const savePassPopup = await driver.$('id=android:id/autofill_save');
     const popupVisible = await savePassPopup.isDisplayed().catch(() => false);
         if (!popupVisible)  return false;
-          const neverSave = await driver.$('id=android:id/autofill_save_no');
-          await neverSave.click();
-          console.log("dismissed save password popup");
+        const neverSave = await driver.$('id=android:id/autofill_save_no');
+        await neverSave.click();
+        console.log("dismissed save password popup");
 }
